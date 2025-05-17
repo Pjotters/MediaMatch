146ca181cff4d28db6aa892ef5f52383c624f191
Pjotters-Chain <05.hek_domein@icloud.com>
2025-05-17 20:30:29 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // MediaMatch Frontend config.js
 window.config = {
-  authApiUrl: 'https://motherboard-pages-nested-missed.trycloudflare.com', // Auth & abonnementen
-  photoApiUrl: 'https://tommy-blah-separated-study.trycloudflare.com', // Lokale backend voor foto-upload/gezicht
+  authApiUrl: 'https://dose-handmade-downtown-labels.trycloudflare.com', // Auth & abonnementen
+  photoApiUrl: 'https://considerations-pork-clear-networking.trycloudflare.com', // Lokale backend voor foto-upload/gezicht
   riffusionUrl: 'https://beautifully-specialties-architectural-tourism.trycloudflare.com'
 };
